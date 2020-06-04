f3472f0bbff382983a4e2657714334aef0f28fc7
Thlimass <thlima.silva.souza@gmail.com>
2020-06-04 10:18:48 -0300

    Incluindo mais um Objeto na Lista.

--- src/database/carDatabaseMock.js
+++ src/database/carDatabaseMock.js
@@ -1,54 +1,107 @@
-const mock = {
-
-    "status": true,
-    "result": {
-        "veiculo": {
-            "placa": "LSO6I92",
-            "renavam": "01089855947"
-        },
-        "multas": [
-            {
-                "ait": "K30462058",
+const mock = [
+    {
+        "status": true,
+        "result": {
+            "veiculo": {
                 "placa": "LSO6I92",
-                "cpf_cnpj": "056.588.037-30",
-                "valor_original": 203.03,
-                "valor": 203.03,
-                "vencimento": "05/08/2019",
-                "status_pagamento": "vencida",
-                "situacao": "não disponível",
-                "renainf": "não disponível",
-                "data_pgto_desconto": "não disponível",
-                "enquadramento": "não disponível",
-                "data_infracao": "não disponível",
-                "hora_infracao": "não disponível",
-                "descricao": "não disponível",
-                "local": "não disponível",
-                "orgao_emissor": "não disponível",
-                "agente_emissor": "não disponível"
-            }
-        ],
-        "ipvas": [
-            {
-                "ano": 2020,
-                "valor": 1450.15
-            }
-        ],
-        "licenciamentos": [
-            {
-                "ano": 2020,
-                "valor": 210.48
-            }
-        ],
-        "dpvats": [
-            {
-                "ano": "2020",
-                "valor": 5.23
-            }
-        ],
-        "dividaativa": []
-    }
+                "renavam": "01089855947"
+            },
+            "multas": [
+                {
+                    "ait": "K30462058",
+                    "placa": "LSO6I92",
+                    "cpf_cnpj": "056.588.037-30",
+                    "valor_original": 203.03,
+                    "valor": 203.03,
+                    "vencimento": "05/08/2019",
+                    "status_pagamento": "vencida",
+                    "situacao": "não disponível",
+                    "renainf": "não disponível",
+                    "data_pgto_desconto": "não disponível",
+                    "enquadramento": "não disponível",
+                    "data_infracao": "não disponível",
+                    "hora_infracao": "não disponível",
+                    "descricao": "não disponível",
+                    "local": "não disponível",
+                    "orgao_emissor": "não disponível",
+                    "agente_emissor": "não disponível"
+                }
+            ],
+            "ipvas": [
+                {
+                    "ano": 2020,
+                    "valor": 1450.15
+                }
+            ],
+            "licenciamentos": [
+                {
+                    "ano": 2020,
+                    "valor": 210.48
+                }
+            ],
+            "dpvats": [
+                {
+                    "ano": "2020",
+                    "valor": 5.23
+                }
+            ],
+            "dividaativa": []
+        }
 
-}
+    },
+    {
+        "status": true,
+        "result": {
+            "veiculo": {
+                "placa": "KXY7559",
+                "renavam": "01089453688"
+            },
+            "multas": [
+                {
+                    "ait": "K30913221",
+                    "placa": "KXY7559",
+                    "cpf_cnpj": "122.588.438-55",
+                    "valor_original": 203.03,
+                    "valor": 203.03,
+                    "vencimento": "05/08/2019",
+                    "status_pagamento": "vencida",
+                    "situacao": "não disponível",
+                    "renainf": "não disponível",
+                    "data_pgto_desconto": "não disponível",
+                    "enquadramento": "não disponível",
+                    "data_infracao": "não disponível",
+                    "hora_infracao": "não disponível",
+                    "descricao": "não disponível",
+                    "local": "não disponível",
+                    "orgao_emissor": "não disponível",
+                    "agente_emissor": "não disponível"
+                }
+            ],
+            "ipvas": [
+                {
+                    "ano": 2020,
+                    "valor": 1450.15
+                }
+            ],
+            "licenciamentos": [
+                {
+                    "ano": 2020,
+                    "valor": 210.48
+                }
+            ],
+            "dpvats": [
+                {
+                    "ano": "2020",
+                    "valor": 5.23
+                }
+            ],
+            "dividaativa": []
+        }
+
+    }
+]
+
+
 module.exports = new class Database {
     constructor(Car) {
         this.Car = mock
@@ -56,5 +109,6 @@ module.exports = new class Database {
 
     find() {
         return mock
-    }
+    };
+
 }
