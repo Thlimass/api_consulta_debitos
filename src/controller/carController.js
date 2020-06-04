c61c9714587471d0d18b5cc3742178555b980e0b
Thlimass <thlima.silva.souza@gmail.com>
2020-06-04 12:23:41 -0300

    Colocando Cache com Redis na busca feita por placa.

--- src/controller/carController.js
+++ src/controller/carController.js
@@ -5,12 +5,13 @@ const CarRepository = require('../repository/carRepository');
 const redis = require('redis');
 const client = redis.createClient();
 
-exports.get = (req, res ) => {
+exports.get = (req, res) => {
 
     client.get('allCars', function (err, reply) {
         if (reply) {
+            const allCars = JSON.parse(reply)
             console.log('redis');
-            res.send(reply)
+            res.send(allCars)
         } else {
             console.log('db');
             CarRepository.getAll()
@@ -26,8 +27,22 @@ exports.get = (req, res ) => {
 
 
 exports.getByPlateNumber = (req, res) => {
-    CarRepository.getByPlateNumber(req.params.placa)
-        .then((car) => {
-            res.status(200).send(car);
-        }).catch(err => res.status(500).send(err))
+    client.get('car', function (err, carCached) {
+        if (carCached) {
+            const car = JSON.parse(carCached);
+            if (car[0].result.veiculo.placa === req.params.placa) {
+                console.log('redis');
+                res.send(car);
+            }
+        } else {
+            console.log('db');
+            CarRepository.getByPlateNumber(req.params.placa)
+                .then((car) => {
+                    client.set('car', JSON.stringify(car));
+                    client.expire('car', 5);
+                    res.status(200).send(car);
+                }).catch(err => res.status(500).send(err))
+        }
+    });
+
 };
